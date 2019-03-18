5d755f8f03d0e4cb4509a7c6faa29a7d691088f3
Daniel Yoo <daniel.yoo12321@gmail.com>
2019-03-17 23:14:25 -0700

    html structure of moviedetails.php

--- moviedetails.php
+++ moviedetails.php
@@ -1,11 +1,93 @@
 <html>
-<head><title>Movie Details</title></head>
+<head>
+	<style>
+		.info {
+			border: 2px solid black;
+			width: 300px;
+		}
+		.review {
+			margin-top: 10px;
+			border: 2px solid black;
+			width: 300px;
+			height: 200px;
+			overflow: scroll;
+		}
+		.comment {
+			margin-top: 10px;
+			border: 2px solid black;
+			width: 300px;
+			text-align: center;
+		} 
+		#anchor {
+			text-decoration: none;
+		}
+	</style>
+	<title>Movie Details</title>
+</head>
 <body>
-
 <?php
-	print "<h2>Movie Details Page</h2><br />";
-
+	print "<h2>Movie Details Page</h2><br/>";
 ?>
-
+	<h2>Movie Title</h2>
+	<div class="thumbnail">
+	</div>
+	<div class="info">
+		<div class="actors">
+			<p>Actors: Daniel, Joban, Daniel, Joban, Daniel, Joban, Daniel, Joban</p>
+		</div>
+		<div class="description">
+			<p>Description: Daniel and Joban and Daniel and Joban</p>
+		</div>
+	</div>
+	<div class="comment">
+		<a href="#a" id="anchor">Comment</a>
+	</div>
+	<div class="review">
+		<div>
+			User 1: 10/10
+		</div>
+		<div>
+			User 2: 10/10
+		</div>
+		<div>
+			User 3: 10/10
+		</div>
+		<div>
+			User 4: 10/10
+		</div>
+		<div>
+			User 5: 10/10
+		</div>
+		<div>
+			User 6: 10/10
+		</div>
+		<div>
+			User 7: 10/10
+		</div>
+		<div>
+			User 8: 10/10
+		</div>
+		<div>
+			User 9: 10/10
+		</div>
+		<div>
+			User 10: 10/10
+		</div>
+		<div>
+			User 11: 10/10
+		</div>
+		<div>
+			User 12: 10/10
+		</div>
+		<div>
+			User 13: 10/10
+		</div>
+		<div>
+			User 14: 10/10
+		</div>
+		<div>
+			User 15: 10/10
+		</div>
+	</div>
 </body>
 </html>
